1da7f37ac00aec71adac38a33c9cac9d56d2ff0f
Adam Ribaudo <adam.ribaudo@gmail.com>
2020-01-22 14:47:14 -0500

    updated table name for BQ

--- KKBox R Project/sample BigQuery query.R
+++ KKBox R Project/sample BigQuery query.R
@@ -4,23 +4,23 @@ library(tidyverse)
 bq_auth(path = "ds5500-7e5f8aa07468.json")
 billing <- "ds5500"
 
-sql <- "SELECT date FROM `ds5500.kkbox.user_logs_dataprep` limit 100"
+sql <- "SELECT msno, count(msno) FROM `ds5500.kkbox.user_logs_for_members_existed_before_2016` 
+group by msno order by count(msno) desc LIMIT 1000"
 tb <- bq_project_query(billing, sql)
-df_example <- bq_table_download(tb)
-df_example
-
-sql <- "SELECT msno, COUNT(*) as member_count FROM `ds5500.kkbox.user_logs_dataprep` 
-  group by msno order by member_count desc limit 100"
-tb <- bq_project_query(billing, sql)
-df_log_count_by_member <- bq_table_download(tb, max_results = 100)
+df_log_count_by_member <- bq_table_download(tb)
 df_log_count_by_member
 
-sql <- "SELECT * FROM `ds5500.kkbox.user_logs_dataprep` 
+sql <- "SELECT * FROM `ds5500.kkbox.members_exist_before_2016` 
   where msno = 'w/7CiqS+abkc6DQ6cW+4fdTy4VvBeezvuHAkSJSFNq0='"
 tb <- bq_project_query(billing, sql)
 df_most_logs <- bq_table_download(tb)
 df_most_logs
 
+sql <- "SELECT * FROM `ds5500.kkbox.members_exist_before_2016`"
+tb <- bq_project_query(billing, sql)
+members <- bq_table_download(tb)
+#write_csv(members, "members_existed_before_20160101.csv")
+
 #TODO: 
-# - find all members with 1yr of user_log & transaction data before evaluation window
-# - perform undersampling related to class imbalance
+# perform undersampling related to class imbalance
+
